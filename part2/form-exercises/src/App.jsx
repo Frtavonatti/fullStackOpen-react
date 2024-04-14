b8c9a98aa4aca22e315443759043b783120ecdf9
frtavonatti <frtavonatti@gmail.com>
2024-04-14 09:24:14 +0100

    Ejercicio 2.6 terminado

--- part2/form-exercises/src/App.jsx
+++ part2/form-exercises/src/App.jsx
@@ -3,28 +3,43 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' },
   ]) 
   const [newName, setNewName] = useState('')
 
-  // const handleClick = () => {
-    
-  // }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    }
+    newName === '' 
+    ? console.log('Agrega mas información') 
+    : setPersons(persons.concat(newPerson));
+    setNewName('')
+  }
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button
+            style={{backgroundColor: 'green', marginTop: '1rem'}}
+            onClick={handleSubmit} type="submit">
+              add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => <li> {person.name} </li> )}
+        {persons.map((person) => <li key={person.name}> {person.name} </li> )}
       </ul>
     </div>
   )
