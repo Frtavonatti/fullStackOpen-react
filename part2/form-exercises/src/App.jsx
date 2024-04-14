aeb3fcc4f176eb8067f1fd30da1c50d83afdb3b3
frtavonatti <frtavonatti@gmail.com>
2024-04-14 09:58:21 +0100

    Ejercicio 2.8 terminado

--- part2/form-exercises/src/App.jsx
+++ part2/form-exercises/src/App.jsx
@@ -5,15 +5,18 @@ const App = () => {
   const [persons, setPersons] = useState([
     { 
       name: 'Arto Hellas',
+      number: 999999,
       id: 0 
     },
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const newPerson = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     }
 
@@ -27,8 +30,12 @@ const App = () => {
     setNewName('')
   }
 
-  const handleInputChange = (event) => {
+  const handleNameInputChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberInputChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,7 +43,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleInputChange}/>
+          name: <input value={newName} onChange={handleNameInputChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberInputChange}/>
         </div>
         <div>
           <button
@@ -48,7 +58,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => <li key={person.id}> {person.name} </li> )}
+        {persons.map((person) => <li key={person.id}> {person.name} : {person.number} </li> )}
       </ul>
     </div>
   )
