117dba3a197f963322a8c57ed9326ebb6d6ffe14
frtavonatti <frtavonatti@gmail.com>
2024-04-14 17:19:13 +0100

    Parte 2c: Ejercicio 2.11 terminado

--- part2/getFromServer/src/App.jsx
+++ part2/getFromServer/src/App.jsx
@@ -8,11 +8,9 @@ function App() {
   const [showAll, setShowAll] = useState(true)
 
   const hook = () => {
-    // console.log('effect')
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        // console.log('promise fulfilled')
         setNotes(response.data)
       })
   }
