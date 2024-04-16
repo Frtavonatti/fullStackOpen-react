09387130b433dfa5cd9bd6f327b419daa6c006a5
frtavonatti <frtavonatti@gmail.com>
2024-04-16 21:05:17 +0100

    Se implementa funcionalidad para cambiar importancia de las notas desde la interfaz

--- part2/getFromServer/src/App.jsx
+++ part2/getFromServer/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
+import { Note } from './Components/Note'
 
 function App() {
   const [notes, setNotes] = useState([])
@@ -23,7 +24,6 @@ function App() {
 
   const addNote = event => {
     event.preventDefault()
-    console.log(newNote);
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
@@ -38,11 +38,29 @@ function App() {
       setNewNote('')
   }
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
   return (
     <>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => <li key={note.id}> {note.content} </li> )}
+        {/* {notes.map((note) => <li key={note.id}> {note.content} </li> )} */}
+        {notes.map((note, i) => 
+          <Note
+          key={i}
+          note={note} 
+          style= {note.style} 
+          toggleImportance={() => toggleImportanceOf(note.id)}
+        />
+      )}
       </ul>
 
     <form>
--- part2/getFromServer/src/Components/Note.jsx
+++ part2/getFromServer/src/Components/Note.jsx
@@ -1,7 +1,15 @@
-const Note = ({ note }) => {
-    return (
-      <li>{note.content}</li>
-    )
-  }
-  
-  export default Note
+const Note = ({ note, style, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
+
+  return (
+    <li>
+      {note.content} 
+      <button 
+        style= {note.important ? {color: 'green', margin:'0.5rem'} : {color: 'red', margin:'0.5rem'}}
+        onClick={toggleImportance}>{label}</button>
+    </li>
+  )
+}
+
+export { Note }
