aa6af8cc1c7eb7fa53baa0d5de7861dc6c6446be
frtavonatti <frtavonatti@gmail.com>
2024-04-30 20:07:16 +0100

    Part 3: Ejercicio 3.4 completado

--- part3-exercises/index.js
+++ part3-exercises/index.js
@@ -2,7 +2,7 @@ const { error } = require('console')
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -49,6 +49,12 @@ app.get('/api/persons/:id', (req, res) => {
         .end()
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+}) 
 
 const PORT = 3001
 app.listen(PORT, () => {
